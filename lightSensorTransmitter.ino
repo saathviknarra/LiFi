6d747200a07494f75d04a07654ec40d0e7f8691b
Jeff Ma <jeffmayuan@outlook.com>
2021-10-09 19:59:17 -0500

    Update lightSensorTransmitter.ino

--- lightSensorTransmitter.ino
+++ lightSensorTransmitter.ino
@@ -1,20 +1,70 @@
-int green = 11;
-int red = 12;
+int green = 12;
+int red = 11;
+int ackPin = 10;
+int ackData;
+int startSigPin = 9;
+int precode = 13;
+int sendData;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(green, OUTPUT);
   pinMode(red, OUTPUT);
+  pinMode(ackPin, INPUT);
+  pinMode(startSigPin, INPUT);
+  Serial.begin(9600); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(green, HIGH);
-  delay(10);
-  digitalWrite(green, LOW);
-  delay(10);
+void sendOne(){
   digitalWrite(red, HIGH);
-  delay(10);
+  delay(50);
+}
+void sendZero(){
   digitalWrite(red, LOW);
-  delay(10);
-  //exit(0);
+  delay(50);  
+}
+void sendPrecode(){
+   sendData = precode;
+   while(sendData){
+       Serial.println(sendData&1);
+       if(sendData & 1){
+          digitalWrite(red, HIGH);
+          delay(50);
+          digitalWrite(red, LOW);
+       } else {
+          digitalWrite(red, LOW);
+          delay(50); 
+       }   
+       sendData=sendData>>1;  
+   }
+}
+
+
+void loop() {
+  Serial.print("START");
+  // wait for the switch to turn on once
+  while(1){
+    if(digitalRead(startSigPin)==0)
+      break;
+  }
+  Serial.print("START Calibration...");
+  // 0-1-alternation until signified by the sender
+  while(1) {
+    ackData=digitalRead(ackPin);
+    if (ackData){
+      break;
+    }
+    digitalWrite(red, HIGH);
+    delay(50);
+    digitalWrite(red, LOW);
+    delay(50);
+    //exit(0);
+  }
+  Serial.print("Calibration done!!!");
+  Serial.print("\n");
+  digitalWrite(red, LOW);
+  delay(5000);
+  sendPrecode();
+  while(1);
+
 }
