9150e97e4bb140ac61831fe1d1d8f760f7723291
saathviknarra <saathvik_narra@yahoo.com>
2021-12-02 20:12:18 -0600

    Update lightSensorTransmitter.ino

--- lightSensorTransmitter.ino
+++ lightSensorTransmitter.ino
@@ -4,11 +4,11 @@ int ackPin = 10;
 int ackData;
 int startSigPin = 9;
 int precode = 21;
+const char msg[] = "Hello World!";
 int msgIdx = 0;
-#define MSG_LEN 15
-#define DELAY 30
-char msg[15] = "Hello World!";
-//char msg[15] = "rrrrrrrrrrrr";
+int precodeIdx = 105;
+int fullMesIdx = 5;
+bool fullmessage[5+(5*sizeof(msg))+((5*sizeof(msg))/100)*5] = {0};
 char encoding[16] = { \
   0b01111, 0b10010, 0b00101, 0b10101, \
   0b01010, 0b11010, 0b01110, 0b11110, \
@@ -17,6 +17,8 @@ char encoding[16] = { \
   };
 int charEncoded;
 int charBuffer;
+int firsthalf;
+int secondhalf;
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,6 +27,48 @@ void setup() {
   pinMode(ackPin, INPUT);
   pinMode(startSigPin, INPUT);
   Serial.begin(9600);
+  fullmessage[0] = 1;
+  fullmessage[1] = 0;
+  fullmessage[2] = 1;
+  fullmessage[3] = 0;
+  fullmessage[4] = 1;
+  while((msgIdx % 20 != 0 || msgIdx == 0) && (msgIdx < sizeof(msg))) {
+    if(fullMesIdx == precodeIdx){
+      fullmessage[fullMesIdx] = 1;
+      fullmessage[fullMesIdx+1] = 0;
+      fullmessage[fullMesIdx+2] = 1;
+      fullmessage[fullMesIdx+3] = 0;
+      fullmessage[fullMesIdx+4] = 1;
+      precodeIdx = precodeIdx + 105;
+      fullMesIdx = fullMesIdx + 5;
+    }
+    charBuffer = msg[msgIdx];
+    firsthalf = encoding[(charBuffer&0xf)];
+    secondhalf = encoding[(charBuffer>>4)];
+    int bitNum = 0;
+    while(bitNum<5){
+      if(firsthalf & 1){
+         fullmessage[fullMesIdx] = 1;
+      }else{
+         fullmessage[fullMesIdx] = 0;
+      }
+      fullMesIdx++;
+      bitNum++;
+      firsthalf  = firsthalf >> 1;
+    }
+    bitNum = 0;
+    while(bitNum<5){
+      if(secondhalf & 1){
+         fullmessage[fullMesIdx] = 1;
+      }else{
+         fullmessage[fullMesIdx] = 0;
+      }
+      fullMesIdx++;
+      bitNum++;
+      secondhalf  = secondhalf >> 1;
+    }
+    msgIdx++;
+  }
 }
 
 //void sendOne(){
